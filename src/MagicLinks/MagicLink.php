86bc3ec700787ee62f2a788977b7dd5238c62e0d
Craig Wann <craig@codezone.io>
2024-06-17 13:30:31 -0500

    Adding magic link.

---
+++ src/MagicLinks/MagicLink.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace CodeZone\DT\MagicLinks;
+
+use CodeZone\DT\Factories\ContainerFactory;
+use League\Route\Http\Exception\NotFoundException;
+use League\Route\Router;
+use CodeZone\DT\Services\RouteInterface;
+
+abstract class MagicLink extends DT_Magic_Url_Base {
+    private static $_instance = null;
+    public $page_title = 'Magic App';
+    public $page_description = 'Magic Link';
+    public $root = 'magic';
+    public $type = 'app';
+    public $post_type = 'user';
+    public $show_bulk_send = false;
+    public $show_app_tile = false;
+    public $meta = [];
+    private $meta_key = ''; // Allows for instance specific data.
+
+    public function __construct() {
+        /**
+         * Specify metadata structure, specific to the processing of current
+         * magic link type.
+         *
+         * - meta:              Magic link plugin related data.
+         *      - app_type:     Flag indicating type to be processed by magic link plugin.
+         *      - post_type     Magic link type post type.
+         *      - contacts_only:    Boolean flag indicating how magic link type user assignments are to be handled within magic link plugin.
+         *                          If True, lookup field to be provided within plugin for contacts only searching.
+         *                          If false, Dropdown option to be provided for user, team or group selection.
+         *      - fields:       List of fields to be displayed within magic link frontend form.
+         */
+        $this->meta = [
+            'app_type'      => 'magic_link',
+            'post_type'     => $this->post_type,
+            'contacts_only' => false,
+            'fields'        => [
+                [
+                    'id'    => 'name',
+                    'label' => 'Name'
+                ]
+            ]
+        ];
+
+        $this->meta_key  = $this->root . '_' . $this->type . '_magic_key';
+
+        $this->init();
+
+        parent::__construct();
+
+        /**
+         * user_app and module section
+         */
+        add_filter( 'dt_settings_apps_list', [ $this, 'dt_settings_apps_list' ], 10, 1 );
+        add_action( 'rest_api_init', [ $this, 'add_endpoints' ] );
+
+        /**
+         * tests if other URL
+         */
+        $url = dt_get_url_path();
+        if ( strpos( $url, $this->root . '/' . $this->type ) === false ) {
+            return;
+        }
+        /**
+         * tests magic link parts are registered and have valid elements
+         */
+        if ( ! $this->check_parts_match() ) {
+            return;
+        }
+
+        $this->boot();
+    } // End instance()
+
+    public function boot() {
+        // Extend this function to add custom boot actions
+    }
+
+    public function init() {
+        // Extend this function to add custom initialization
+    }
+
+    public static function instance() {
+        if ( is_null( self::$_instance ) ) {
+            self::$_instance = new static();
+        }
+
+        return self::$_instance;
+    }
+
+    public function whitelist_current_route() {
+        $this->type_actions[ $this->get_current_action() ] = 'Current Route';
+    }
+
+    public function get_current_action() {
+        $current_action = request()->getUri()->getPath();
+        $current_action = trim( $current_action, '/' );
+        $urlParts = explode( '/', $current_action );
+        $requiredParts = array_slice( $urlParts, 3, 1 );
+        return implode( '/', $requiredParts );
+    }
+
+    public function add_endpoints() {
+        // Extend this function to add custom endpoints
+    }
+
+    /**
+     * Renders the response for the current request using the router and renderer actions
+     *
+     * @return void
+     */
+    public function render() {
+        $route = ContainerFactory::singleton()->get( RouteInterface::class );
+        $route->with_routes( function ( Router $r ) {
+            $this->routes( $r );
+        } );
+
+        if ( WP_DEBUG ) {
+            $route->dispatch();
+        } else {
+            try {
+                $route->dispatch();
+            } catch ( NotFoundException $e ) {
+                wp_die( esc_html( $e->getMessage() ), esc_attr( $e->getCode() ) );
+            }
+        }
+
+        $route->render();
+    }
+
+    public function print_scripts() {
+    }
+
+    public function print_styles() {
+    }
+
+    public function routes( Router $r ) {
+        // Extend this function to add custom routes
+    }
+}
---
+++ src/Services/Route.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace CodeZone\DT\Services;
+
+use CodeZone\DT\Factories\ServerRequestFactory;
+use League\Route\Router;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * Class Route
+ *
+ * Represents a route in the application.
+ * @see https://route.thephpleague.com/4.x/usage/
+ */
+class Route implements RouteInterface
+{
+    /**
+     * @var Router $router
+     * The Router Instance.
+     * @see https://route.thephpleague.com/4.x/usage/
+     */
+    protected $router;
+
+    /**
+     * The $request variable holds the HTTP request data.
+     * @see https://www.php-fig.org/psr/psr-7/
+     *
+     * @var ServerRequestInterface | null
+     */
+    protected $request;
+
+    /**
+     * The $response variable holds the HTTP response data.
+     * @see https://www.php-fig.org/psr/psr-7/
+     *
+     * @var ResponseInterface | null
+     */
+    protected $response;
+
+    /**
+     * The $renderer variable is used to generate HTML or JSON output from a Response.
+     *
+     * @var ResponseRenderer
+     */
+    protected $renderer;
+
+    /**
+     * Class constructor.
+     *
+     * @param Router $router The router instance.
+     * @param ServerRequestInterface $request The server request instance.
+     * @param ResponseRenderer $renderer The response renderer instance.
+     */
+    public function __construct( Router $router, ServerRequestInterface $request, ResponseRenderer $renderer ) {
+        $this->router = $router;
+        $this->request = $request;
+        $this->renderer = $renderer;
+    }
+
+    /**
+     * Initialize the router with a specified URI instead of relying on the REQUEST_URI.
+     * This is useful for testing or for scp[omg the router to a subdirectory.
+     *
+     * @param string $uri The URI to set for the request.
+     * @return self The instance of the class.
+     * @eee https://docs.laminas.dev/laminas-diactoros/
+     */
+    public function as_uri( $uri ): self {
+        return $this->with_request(
+            ServerRequestFactory::with_uri( $uri )
+        );
+    }
+
+    /**
+     * Adds middleware to the router.
+     *
+     * @param mixed $middleware The middleware to add. Can be a string or an array of strings.
+     *
+     * @return self The instance of the class.
+     */
+    public function with_middleware( $middleware ): self {
+
+        if ( is_array( $middleware ) ) {
+            foreach ( $middleware as $m ) {
+                $this->router->middleware( $m );
+            }
+            return $this;
+        } else {
+            $this->router->middleware( $middleware );
+        }
+
+        return $this;
+    }
+
+
+    /**
+     * Sets the request for the route.
+     *
+     * @param mixed $request The request for the route.
+     * @return self The instance of the class.
+     */
+    public function with_request( $request ): self {
+        $this->request = $request;
+        return $this;
+    }
+
+    /**
+     * Adds routes to the router.
+     *
+     * @param callable $register_routes The callback function that registers routes to the router.
+     * @return self The instance of the class.
+     */
+    public function with_routes( callable $register_routes ): self {
+        $register_routes( $this->router );
+        return $this;
+    }
+
+    /**
+     * Loads routes from a file and adds them to the route collector.
+     *
+     * @param string $file The path to the file containing the routes.
+     * @return self Returns an instance of the current class.
+     */
+    public function from_file( $file ): self {
+        return $this->with_routes( function ( $r ) use ( $file ) {
+            require_once $file;
+        });
+    }
+
+    /**
+     * Dispatches the request to the router and sets the response.
+     *
+     * @return self Returns an instance of the current class.
+     */
+    public function dispatch(): self {
+        $this->response = $this->router->dispatch( $this->request );
+        return $this;
+    }
+
+    /**
+     * Renders the response using the renderer if it exists.
+     * If the response is not set, it dispatches the request to the router first.
+     *
+     * @return self Returns an instance of the current class.
+     */
+    public function render(): self {
+        if ( !$this->response ) {
+          $this->dispatch();
+        }
+
+        if ( $this->response ) {
+            $this->renderer->render( $this->response );
+        }
+
+        return $this;
+    }
+}
